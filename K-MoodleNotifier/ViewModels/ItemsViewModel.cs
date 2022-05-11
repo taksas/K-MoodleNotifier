25f186097acd441b795798a987a0e199dac53663
taksas <takuminakayama4869@gmail.com>
2022-05-11 15:32:37 +0900

    Notify Days CheckBox Added.

--- K-MoodleNotifier/ViewModels/ItemsViewModel.cs
+++ K-MoodleNotifier/ViewModels/ItemsViewModel.cs
@@ -24,6 +24,9 @@ namespace K_MoodleNotifier.ViewModels
 
 
 
+
+
+
     }
 
 }
--- K-MoodleNotifier/Views/ItemsPage.xaml.cs
+++ K-MoodleNotifier/Views/ItemsPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -27,6 +28,45 @@ namespace K_MoodleNotifier.Views
         void LoginCommand(object sender, System.EventArgs e)
         {
             Shell.Current.GoToAsync(nameof(NewItemPage));
+        }
+
+        async void OnCheckBoxCheckedChanged1(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                await SecureStorage.SetAsync("Day1", "1");
+                // var text1 =  await SecureStorage.GetAsync("Day1");
+                // Debug.WriteLine(text1);
+            }
+            else
+            {
+                await SecureStorage.SetAsync("Day1", "0");
+                // var text1 = await SecureStorage.GetAsync("Day1");
+                // Debug.WriteLine(text1);
+            }
+            
+        }
+        async void OnCheckBoxCheckedChanged2(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                await SecureStorage.SetAsync("Day2", "1");
+            }
+            else
+            {
+                await SecureStorage.SetAsync("Day2", "0");
+            }
+        }
+        async void OnCheckBoxCheckedChanged3(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                await SecureStorage.SetAsync("Day3", "1");
+            }
+            else
+            {
+                await SecureStorage.SetAsync("Day3", "0");
+            }
         }
     }
 }
